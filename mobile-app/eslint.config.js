99318bceac2870cab7e96a392c1f12e6b8b74aa2
binit <binitgupta.1711@gmail.com>
2025-09-14 01:09:49 +0530

    -installed react native and changed it default configuration from  tsx to jsx

---
+++ mobile-app/eslint.config.js
@@ -0,0 +1,18 @@
+// https://docs.expo.dev/guides/using-eslint/
+const { defineConfig } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
+
+module.exports = defineConfig([
+  expoConfig,
+  {
+    ignores: ['dist/*'],
+    files: ['**/*.js', '**/*.jsx'],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    }
+  },
+]);
