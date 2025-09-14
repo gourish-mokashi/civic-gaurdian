1104be4980f613036113827296bcb17c394daf04
binit <binitgupta.1711@gmail.com>
2025-09-15 01:33:49 +0530

    fixed the out of safe area content problem

--- mobile-app/app/_layout.jsx
+++ mobile-app/app/_layout.jsx
@@ -1,6 +1,18 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import './globals.css';
 
 export default function RootLayout() {
-  return <Stack screenOptions={{headerShown: false}} />;
+  return (
+    <SafeAreaProvider>
+      <Stack 
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: 'transparent' },
+          statusBarStyle: 'auto',
+          statusBarBackgroundColor: 'transparent'
+        }} 
+      />
+    </SafeAreaProvider>
+  );
 }
