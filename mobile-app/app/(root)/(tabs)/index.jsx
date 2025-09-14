52fdf89e22e9488951016d6ff06082012c971322
binit <binitgupta.1711@gmail.com>
2025-09-14 10:29:48 +0530

    basic routing and pages setup

--- mobile-app/app/(root)/(tabs)/index.jsx
+++ mobile-app/app/(root)/(tabs)/index.jsx
@@ -1,9 +1,12 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-red-500 font-bold">Welcome</Text>
+      <Link href="/SignIn">Sign In</Link>
+      <Link href="/Profile">Profile</Link>
+      <Link href="/Posts">Posts</Link>
     </View>
   );
 }
