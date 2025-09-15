21388403bbf1ddbe4d31bd5fa1acea2918a64083
binit <binitgupta.1711@gmail.com>
2025-09-15 22:21:50 +0530

    demo homepage

---
+++ mobile-app/app/(root)/(tabs)/Home.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const Notification = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-4xl">Home</Text>
+    </View>
+  );
+};
+
+export default Notification;
