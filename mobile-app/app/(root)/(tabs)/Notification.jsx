dd79efa1fed10339d3d85efe12cc1cb8519c88ac
binit <binitgupta.1711@gmail.com>
2025-09-15 22:22:21 +0530

    make demo profile and notification page

---
+++ mobile-app/app/(root)/(tabs)/Notification.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const Notification = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-4xl">Notifications</Text>
+    </View>
+  );
+};
+
+export default Notification;
--- mobile-app/app/(root)/(tabs)/Profile.jsx
+++ mobile-app/app/(root)/(tabs)/Profile.jsx
@@ -1,10 +1,10 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
 
 const Profile = () => {
   return (
-    <SafeAreaView className = "flex-1 justify-center items-center">
-      <Text className = "text-4xl">Profile</Text>
+    <SafeAreaView className="flex-1 justify-center items-center">
+      <Text className="text-4xl">Profile</Text>
     </SafeAreaView>
   )
 }
