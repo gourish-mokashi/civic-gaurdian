82216888421a49c662e3b144bb2e746f2f86a303
binit <binitgupta.1711@gmail.com>
2025-09-15 10:53:59 +0530

    better auth setup

--- mobile-app/app/(root)/(tabs)/Posts.jsx
+++ mobile-app/app/(root)/(tabs)/Posts.jsx
@@ -1,12 +1,15 @@
-import { View, Text } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Posts = () => {
   return (
-    <View>
-      <Text>Posts</Text>
-    </View>
+    <SafeAreaView className = "flex-1 justify-center items-center">
+      <Text className = "text-4xl">Posts</Text>
+    </SafeAreaView>
   )
 }
 
 export default Posts
+
+const styles = StyleSheet.create({})
