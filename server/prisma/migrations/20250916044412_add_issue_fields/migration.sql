2fed39c1423bceb7a9d5eb8fd61168c7e0640a7d
Shreehari-Acharya <shreehari.acharya.06@gmail.com>
2025-09-16 12:01:54 +0530

    New migrations

--- server/prisma/migrations/20250916044412_add_issue_fields/migration.sql
+++
@@ -1,6 +0,0 @@
--- CreateEnum
-CREATE TYPE "public"."department" AS ENUM ('MEDICAL', 'POLICE', 'FIRE_BRIGADE', 'MUNICIPAL', 'ELECTRICITY', 'OTHERS');
-
--- AlterTable
-ALTER TABLE "public"."issue" ADD COLUMN     "assignedTo" "public"."department",
-ADD COLUMN     "category" TEXT;
