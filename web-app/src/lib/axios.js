855f88fdffa88a3a8c2886ae51394fc7d4ae0b4e
Gourish <ugcet2401208@reva.edu.in>
2025-09-16 15:48:14 +0530

    Refactor API calls to use axios instance and enhance issue management functionality

---
+++ web-app/src/lib/axios.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+const axiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE || 'http://localhost:3000',
+    withCredentials: true,
+});
+
+export default axiosInstance;
